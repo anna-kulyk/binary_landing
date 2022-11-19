2499179563b23aaa2e859c9dd7c8015070f8c10e
Anna <kulyk_anna@outlook.com>
2022-11-19 00:28:36 -0500

    modified event handlers, added partners section

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,3 @@
-// let menu = document.querySelector(".menu__body");
-// let menuIcon = document.querySelector(".icon-menu");
-
-// menuIcon.addEventListener('click', burgerMenuOnOff);
-
-// menu.addEventListener('click', (e) => {
-//     if (!menu.classList.contains('active')) return;
-//     if (e.target.nodeName == "A") {
-//         burgerMenuOnOff();
-//     }
-// });
-
-// function burgerMenuOnOff() {
-//     menuIcon.classList.toggle('active');
-//     menu.classList.toggle('active');
-//     document.querySelector("body").classList.toggle('lock');
-// }
-
 function ibg(){
     let elements = document.querySelectorAll('.ibg');
     elements?.forEach(function(item){
@@ -27,3 +9,67 @@ function ibg(){
 }
 
 ibg();
+
+//====================================================================================
+
+const navMenu = document.querySelector('.content__menu');
+const portfolioMenu = document.querySelector('.portfolio__menu');
+
+navMenu.addEventListener('click', (event) => navMenuHandler(event));
+portfolioMenu.addEventListener('click', (event) => portfolioMenuHandler(event));
+
+
+function navMenuHandler(event) {
+    if (event.target.nodeName != 'A') {
+        return;
+    }
+    menuHandler(event.target, navMenu.querySelectorAll('.menu__item'))
+}
+
+function portfolioMenuHandler(event) {
+    if (event.target.nodeName != 'LI') {
+        return;
+    }
+    menuHandler(event.target, portfolioMenu.querySelectorAll('.menu__item'))
+}
+
+function menuHandler(targetElement, menuItems) {
+    for (const element of menuItems) {
+        element.classList.remove('menu__item_active');
+    }
+    targetElement.classList.add('menu__item_active');
+}
+
+//====================================================================================
+
+let portfolioItems = document.querySelectorAll('.portfolio__item');
+document.querySelector('.filter').addEventListener('click', (e) => filterHandler(e));
+
+
+function filterHandler(e) {
+    if (e.target.nodeName != 'LI') {
+        return;
+    }
+    let itemClass = `f_${e.target.dataset.filter}`;
+    if (e.target.dataset.filter == 'all') {
+        portfolioItems.forEach(item => showItem(item))
+    }
+    else {
+        portfolioItems.forEach(item => {
+            if(item.classList.contains(itemClass)) {
+                showItem(item);
+            }
+            else {
+                hideItem(item);
+            }
+        })
+    }
+}
+
+function showItem(item) {
+    item.style.display = 'block';
+}
+
+function hideItem(item) {
+    item.style.display = 'none';
+}
